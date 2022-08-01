d574bab300c9ba5c237c01ff6308024cba54086d
ikenna <ikenna.porter@gmail.com>
2022-08-02 00:22:58 +0200

    Feature 13 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from projects.views import project_list_view
+from projects.views import project_list_view, project_detail_view
 
 urlpatterns = [
     path("", project_list_view, name="list_projects"),
+    path("<int:pk>/project/", project_detail_view, name="show_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
 
@@ -8,6 +8,11 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def project_list_view(request):
-    print(request)
     context = {"projects": Project.objects.filter(members=request.user)}
     return render(request, "projects-list.html", context)
+
+
+def project_detail_view(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {"project": project}
+    return render(request, "project_detail.html", context)
