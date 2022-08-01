934fc9c9b75908df6933d855084203f211781e75
ikenna <ikenna.porter@gmail.com>
2022-08-02 00:57:42 +0200

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from projects.views import project_list_view, project_detail_view
+from projects.views import (
+    project_list_view,
+    project_detail_view,
+    CreateProject,
+)
 
 urlpatterns = [
     path("", project_list_view, name="list_projects"),
     path("<int:pk>/project/", project_detail_view, name="show_project"),
+    path("create/", CreateProject.as_view(), name="create_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
 @login_required
@@ -12,7 +12,22 @@ def project_list_view(request):
     return render(request, "projects-list.html", context)
 
 
+@login_required
 def project_detail_view(request, pk):
     project = Project.objects.get(pk=pk)
     context = {"project": project}
     return render(request, "project_detail.html", context)
+
+
+class CreateProject(LoginRequiredMixin, CreateView):
+    model = Project
+    fields = ["name", "description", "members"]
+    template_name = "create_project.html"
+
+    def form_valid(self, form):
+        if form.is_valid:
+            form.save()
+            return redirect("show_project", pk=self.request.user.id)
+
+    def get_success_url(self):
+        return reverse_lazy("show_project", args=self.object.id)
